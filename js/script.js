da0a792dbf70ad2af542ca6cdb816c0dc5b03ef2
netpro <quanganhnguyen2382001@gmail.com>
2021-12-28 21:04:00 +0700

    up

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 1. Thêm và lưu giá trị 
+let btnLuu = document.getElementById("btnLuu");
+let btnReset = document.getElementById("btnReset");
+btnLuu.addEventListener('click',function(){
+    let ho_va_ten       = document.getElementById('txthoten');
+    let dia_chi           = document.getElementById('txtdiachi');
+    let tel             = document.getElementById('txttel');
+    let khach_viet_nam   = document.getElementById('rdokhachvietnam');
+    let nguoi_lon   = document.getElementById('numnguoilon');
+    let tre_em   = document.getElementById('tre_em');
+    
